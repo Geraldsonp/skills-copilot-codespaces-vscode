b6e062d6677efa5d27656d87e5f21356d37aadcb
Geraldson Perez <31606909+Geraldsonp@users.noreply.github.com>
2025-03-05 16:35:36 +0000

    Refactor server setup to use Express and include necessary modules

--- comments.js
+++ comments.js
@@ -1,30 +1,7 @@
 //create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var comments = [];
-var server = http.createServer(function(req, res) {
-    var urlObj = url.parse(req.url, true);
-    var pathname = urlObj.pathname;
-    if (pathname === '/') {
-        fs.readFile('./index.html', function(err, data) {
-            if (err) {
-                res.end('404');
-            } else {
-                res.end(data);
-            }
-        });
-    }
-    if (pathname === '/getComments') {
-        var str = JSON.stringify(comments);
-        res.end(str);
-    }
-    if (pathname === '/submitComment') {
-        var comment = urlObj.query;
-        comments.push(comment);
-        res.end('success');
-    }
-});
-server.listen(8080);
-console.log('server is listening on 8080');
+//require express module
+const express = require('express');
+//require path module
+const path = require('path');
+//require body-parser module
+const bodyParser = require('body-parser ');
