39d17d614026a433ecf35f887ee662b0c837db5c
Geraldson Perez <31606909+Geraldsonp@users.noreply.github.com>
2025-03-05 16:57:48 +0000

    git third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,25 @@
-//create web server
-//require express module
-const express = require('express');
-//require path module
-const path = require('path');
-//require body-parser module
-const bodyParser = require('body-parser ');
+// Create web server
+// Create a web server that listens on port 3000. When you visit the root URL, it should display a list of all the comments. When you visit /new, it should display a form that allows you to submit a new comment. When you submit a new comment, you should be redirected to the root URL.
+// You can use the comments array from the previous exercise.
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var comments = [];
+app.get('/', function (req, res) {
+  res.send(comments.join('<br>'));
+});
+
+app.get('/new', function (req, res) {
+  res.send('<form method="post"><input name="comment"><input type="submit"></form>');
+});
+
+app.post('/new', function (req, res) {
+  comments.push(req.body.comment);
+  res.redirect('/');
+});
+
+app.listen(3000);
